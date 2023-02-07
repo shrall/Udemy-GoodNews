dc8f65465e4cbe3d2636fec63d18422f6d1047f4
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-02-07 22:47:25 +0700

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Marshall Kurniawan on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
